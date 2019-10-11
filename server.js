5c54f2a5558ab06b13815a467852a8d00e7de59c
Volodymyr Yepishev <i.m.bwca@gmail.com>
2019-10-11 20:48:28 +0300

    working draft

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +18,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/timestamp/', getTimestamp);
+app.get('/api/timestamp/:date_string', getTimestamp);
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
-
-
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// return json timestap for the task
+function getTimestamp(req, res) {
+  const dateString = req.params.date_string;
+  const date = dateString ? new Date(dateString) : new Date();
+
+  if (date instanceof Date)
+    res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
+  else
+    res.json({ "unix": null, "utc": "Invalid Date" });
+}
